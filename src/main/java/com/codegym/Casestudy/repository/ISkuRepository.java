08f31403f2361618121c85b0fd349abd24e8564f
VuPham93 <62596412+VuPham93@users.noreply.github.com>
2020-08-01 14:11:51 +0700

    add Img for product

--- src/main/java/com/codegym/Casestudy/repository/ISkuRepository.java
+++ src/main/java/com/codegym/Casestudy/repository/ISkuRepository.java
@@ -2,6 +2,9 @@ package com.codegym.Casestudy.repository;
 
 import com.codegym.Casestudy.model.Sku;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ISkuRepository extends CrudRepository<Sku, Long> {
+//    Sku findByProductProductIdAndAndOptions(Long productId, Long sizeOptionId, Long colorOptionId);
 }
