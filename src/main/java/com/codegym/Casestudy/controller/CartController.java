0836f2dd8a427a1e0ec6d246b2eca5e6c1318388
hungviet276 <viethungkt7k6@gmail.com>
2020-08-02 20:26:28 +0700

    view cart

--- src/main/java/com/codegym/Casestudy/controller/CartController.java
+++ src/main/java/com/codegym/Casestudy/controller/CartController.java
@@ -8,6 +8,7 @@ import com.codegym.Casestudy.service.option.IOptionService;
 import com.codegym.Casestudy.service.product.IProductService;
 import com.codegym.Casestudy.service.sku.ISkuService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mustache.MustacheTemplateAvailabilityProvider;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -32,26 +33,6 @@ public class CartController {
     ISkuService skuService;
     @Autowired
     IOptionService optionService;
-
-//    @GetMapping("/addtocart/{id}")
-//    public ResponseEntity<Product> addToCart(@PathVariable("id") Long id) {
-//        Cart cart = cartService.findCartByUserId(1L);
-//        if (cart != null) {
-//            List<Product> products = (List<Product>) cart.getProducts();
-//            products.add(productService.findById(id).get());
-//            cart.setCartQuantity(1);
-//            cart.setProducts(products);
-//            cartService.save(cart);
-//        } else {
-//            cart.setUserId(1L);
-//            List<Product> products = (List<Product>) cart.getProducts();
-//            products.add(productService.findById(id).get());
-//            cart.setCartQuantity(1);
-//            cart.setProducts(products);
-//            cartService.save(cart);
-//        }
-//        return new ResponseEntity<>(HttpStatus.OK);
-//    }
 
     @GetMapping("/addtocart/{productId}/{sizeOption}/{colorOption}")
     public ResponseEntity<Sku> addToCart(@PathVariable("productId") Long productId,@PathVariable("sizeOption") Long sizeOption,
@@ -80,21 +61,36 @@ public class CartController {
         List<Sku> skus= (List<Sku>) cart.getSkus();
         Map<Product, Map<String, String>> outerMap = new HashMap<>();
         Map<String, String> innerMap = new HashMap<>();
+        double totalPrice = 0;
         for (int i = 0; i < skus.size();i++){
             String size = optionService.findByOptionId(skus.get(i).getOption(1L)).get().getOptionName();
             String color = optionService.findByOptionId(skus.get(i).getOption(2L)).get().getOptionName();
             innerMap.put(size,color);
             outerMap.put(productService.findProductBySkuId(skus.get(i).getSkuId()),innerMap);
+            totalPrice+=productService.findProductBySkuId(skus.get(i).getSkuId()).getPrice();
         }
         ModelAndView modelAndView = new ModelAndView("cart/cart");
         modelAndView.addObject("productCart", outerMap);
         modelAndView.addObject("cart", cart);
+        modelAndView.addObject("totalPrice", totalPrice);
         return modelAndView;
     }
-//
+
+    @GetMapping("/deleteproduct/{productId}/{sizeOption}/{colorOption}")
+    public ModelAndView deleteProduct(@PathVariable("productId") Long productId,@PathVariable("sizeOption") Long sizeOption,
+                                  @PathVariable("colorOption") Long colorOption){
+        Cart cart = cartService.findCartByUserId(1L);
+        List<Sku> skus = (List<Sku>) cart.getSkus();
+        skus.remove(skuService.findByProductIdAndOptions(productId,sizeOption,colorOption));
+        cart.setSkus(skus);
+        cartService.save(cart);
+        ModelAndView modelAndView = new ModelAndView("redirect:/cart/");
+        return modelAndView;
+    }
 //    @GetMapping("/deleteproduct/{id}")
 //    public ModelAndView detleteProduct(@PathVariable("id") Long id) {
 //        Cart cart = cartService.findCartByUserId(1L);
+//
 //        List<Product> products = (List<Product>) cart.getProducts();
 //        products.remove(productService.findById(id));
 //        cart.setProducts(products);
